1915788808509af2b249b3a3084647c87fe2d317
vinod192954 <vvinnu590@gmail.com>
2024-08-27 10:27:16 +0530

    added footer section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import HomePage from './components/HomePage';
 import BlogItemDetailed from './components/BlogItemDetailed';
 import NewBlog from './components/NewBlog';
+import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -15,9 +16,10 @@ function App() {
               <Route exact path="/" component={HomePage}/>
               <Route exact path="/blogs/addBlog" component={NewBlog} />
               <Route exact path="/blogs/:id" component={BlogItemDetailed} />
-              
           </Switch>
+        <Footer/>
       </BrowserRouter>
+
     </div>
   );
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,29 @@
+import { RiTwitterXLine } from "react-icons/ri";
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import "./index.css"
 const Footer=()=>{
     return (
-        <div>
-            
+        <div className="footer-container">
+            <div className="follow-us-section">
+                <h1>Follow us</h1>
+                 <div>
+                    <RiTwitterXLine color="#fff" size={20}/>
+                    <FaFacebook color="#fff" size={20}/>
+                    <FaLinkedin color="#fff" size={20}/>
+                 </div>
+            </div>
+            <div className="contact-us-section">
+                <h1>Contact Us</h1>
+                
+                <p> <span><FaPhoneAlt color="#fff"/></span>+91 6281456842 </p>
+            </div>
+            <div className="address-section">
+                <h1>Address</h1>
+                <p>Kacheguda, near falknama hotel pin code :50212</p>
+            </div>
         </div>
     )
 }
+export default Footer
